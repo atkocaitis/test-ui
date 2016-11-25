1b925de941800a5d7b63400d9e8e66bb554ba195
Algirdas Atkocaitis <Algirdas.Atkocaitis@gfk.com>
2016-11-25 16:57:24 +0100

    add rgb/rgba to hex converter

--- commands/checkLayoutCSS.js
+++ commands/checkLayoutCSS.js
@@ -1,16 +1,36 @@
+'use strict'
+
 /**
 * Function to verify if element css properties are correct.
 * @param  {string} element Element css selector
 * @param  {object} layout Object with expected element size data
 * @return {object} Return this object
 */
-
 var checkLayoutCSS = function(element, layout) {
-  for (var item in layout.css) {
-    this.verify.cssProperty(element, item, layout.css[item]);
+  for (let item in layout.css) {
+    this.getCssProperty(element, item, (result) => {
+      if (layout.css[item].indexOf('rgb') !== -1) {
+        this.verify.equal(rgb2hex(result.value), rgb2hex(layout.css[item]));
+      } else {
+        this.verify.equal(result.value, layout.css[item]);
+      }
+    });
   };
 
   return this;
+};
+
+/**
+* Function to convert color code rgb/rgba to hex.
+* @param  {string} rgb Color code in rgb/rgba
+* @return {string} Return color in hex
+*/
+var rgb2hex = function(rgb) {
+  rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+  return (rgb && rgb.length === 4) ? "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
 };
 
 exports.command = checkLayoutCSS;
